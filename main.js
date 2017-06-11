e20058ead92fa339c515835c4e033acbc6c3eb6b
udaygin <uday.gottapu.in@gmail.com>
2017-06-11 12:27:49 +0530

    Newline fix

--- main.js
+++ main.js
@@ -31,11 +31,13 @@ client.on('message', function (topic, message) {
 	//cleanup the message data 	
 	var message_data = (message instanceof Buffer)? message.toString():message;	
 	message_data = message_data.trim().replace(/\n$/, '');
-
+	if(message_data.startsWith('{') && message_data.endsWith('}')){
+		message_data = JSON.parse(message_data);
+	}
 	console.log('Got message on topic '+ topic,message_data);
 	//filter topics that are not relavant   
 	if(topic.startsWith(mqtt_topic_prefix)){
-		console.log('append it to existing data');
+	//	console.log('append it to existing data');
 		db.push(topic+'[]',message_data);
 	}else{
 		console.log('not a topic that I am supposed to monitor. ignoring');
@@ -52,8 +54,18 @@ app.get('*', (req, res) => {
 	console.log(req.path);
 	var topic_name = req.path.replace(/\/$/, "");
 	//check if a element with that topic name exists in the database 
-	var data = db.getData(topic_name);
-	res.send(data);
+	
+	var data = {};
+	try{
+		data = 	db.getData(topic_name);
+	}catch(ex){
+		//console.log("error",ex);
+	}
+	//console.log('Got from db: ',data);
+	res.setHeader('Content-Type', 'application/json');
+	var response_string = JSON.stringify(data,null,4);
+	//console.log('Sending',response_string);
+	res.send(response_string);
 })
 
 //start listening to any requests that as for the data 
